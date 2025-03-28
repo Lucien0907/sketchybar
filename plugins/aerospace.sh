d0979f3f7f77b1aa54e9c05d61c76407fe3ec300
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-28 03:09:09 +0000

    update

--- plugins/aerospace.sh
+++ plugins/aerospace.sh
@@ -4,11 +4,15 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on label.color=0xff000000 label.drawing=yes icon.drawing=on
+  sketchybar --set $NAME background.drawing=on background.color=0xfff5a97f label.color=0xff000000 label.drawing=yes icon.drawing=on
 else
-  if [ "$(aerospace list-windows --workspace "$1" --count)" = 0 ]; then
-    sketchybar --set $NAME background.drawing=off label.drawing=no icon.drawing=off
+  if [ "$1" = 0 ]; then
+    sketchybar --set $NAME background.drawing=on background.color=0x9924273a label.drawing=yes label.color=0xffffffff icon.drawing=on
   else
-    sketchybar --set $NAME background.drawing=off label.drawing=yes label.color=0xffffffff icon.drawing=on
+    if [ "$(aerospace list-windows --workspace "$1" --count)" -gt 0 ]; then
+      sketchybar --set $NAME background.drawing=off label.drawing=yes label.color=0xffffffff icon.drawing=on
+    else
+      sketchybar --set $NAME background.drawing=off label.drawing=no icon.drawing=off
+    fi
   fi
 fi
