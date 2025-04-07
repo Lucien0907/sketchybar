d5ff7034ad4bcf0e60abbcb884c8c7634e649045
Tao-Wei Chan <taowei.c@outlook.com>
2025-04-07 10:05:46 +0100

    update

--- plugins/aerospace.sh
+++ plugins/aerospace.sh
@@ -3,16 +3,23 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on background.color=0xfff5a97f label.color=0xff000000 label.drawing=yes icon.drawing=on
+source ~/.config/sketchybar/plugins/icon_map.sh
+label="$2"
+while IFS= read -r app_name; do
+  icon=$(get_icon "$app_name")
+  label="$label $icon"
+done < <(aerospace list-windows --workspace "$2" --format "%{app-name}")
+
+if [ "$2" = "$(aerospace list-workspaces --focused)" ]; then
+  sketchybar --set $NAME background.drawing=on background.color=0xfff5a97f label="$label" label.color=0xff000000 label.drawing=yes icon.drawing=on
 else
-  if [ "$1" = 0 ]; then
-    sketchybar --set $NAME background.drawing=on background.color=0x9924273a label.drawing=yes label.color=0xffffffff icon.drawing=on
+  if [ "$2" = "$(aerospace list-workspaces --monitor "$1" --visible)" ]; then
+    sketchybar --set $NAME background.drawing=on background.color=0x99ffffff label="$label" label.color=0xff000000 label.drawing=yes icon.drawing=on
   else
-    if [ "$(aerospace list-windows --workspace "$1" --count)" -gt 0 ]; then
-      sketchybar --set $NAME background.drawing=off label.drawing=yes label.color=0xffffffff icon.drawing=on
+    if [ "$(aerospace list-windows --workspace "$2" --count)" -gt 0 ]; then
+      sketchybar --set $NAME background.drawing=off label.drawing=yes label="$label" label.color=0xffffffff icon.drawing=on
     else
-      sketchybar --set $NAME background.drawing=off label.drawing=no icon.drawing=off
+      sketchybar --set $NAME background.drawing=off label.drawing=no label="$label" icon.drawing=off
     fi
   fi
 fi
---
+++ plugins/icon_map.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# icon_map.sh
+
+DEFAULT_ICON=" "
+get_icon() {
+  local app_name="$1"
+  case "$app_name" in
+  "Arc") echo "󰞍" ;;
+  "Code") echo "󰨞" ;;
+  "Calendar") echo "" ;;
+  "App Store") echo "" ;;
+  "Discord") echo "" ;;
+  "FaceTime") echo "" ;;
+  "Finder") echo "󰀶" ;;
+  "System Settings") echo "" ;;
+  "Google Chrome") echo "" ;;
+  "Messages") echo "" ;;
+  "Preview") echo "" ;;
+  "Spotify") echo "" ;;
+  "Steam Helper") echo "" ;;
+  "TextEdit") echo "" ;;
+  "WeChat") echo "" ;;
+  "Ghostty") echo "" ;;
+  "WeChat") echo "" ;;
+  "Ghostty") echo ="" ;;
+  *) echo "$DEFAULT_ICON" ;;
+  esac
+}
